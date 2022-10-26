e0c2846eab227b359845e95508a182f989675fd9
herniqeu <hhhenriguegodoyyy@gmail.com>
2022-10-26 12:26:34 -0300

    realização das atividades ponderadas desta semana

--- SEMANA_03/03_AUT_EST_ENTREGA/app.js
+++ SEMANA_03/03_AUT_EST_ENTREGA/app.js
@@ -7,11 +7,12 @@ const express = require('express');
     const DBPATH = 'database.db'; //use o nome que você achar melhor para o banco de dados
 
     app.use(express.json());
-    app.get('/habilidade', (req, res) => {
+    app.get('/dados_pessoais', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT * FROM candidato ';
+        console.log(db)
+        var sql = 'SELECT * FROM dados_pessoais';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
@@ -20,3 +21,63 @@ const express = require('express');
         });
         db.close(); // Fecha o banco
     });
+    app.get('/experiencia', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        console.log(db)
+        var sql = 'SELECT * FROM experiencia';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    app.get('/formacao', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        console.log(db)
+        var sql = 'SELECT * FROM formacao';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    app.get('/habilidade', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        console.log(db)
+        var sql = 'SELECT * FROM habilidade';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    app.get('/realizacoes', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        console.log(db)
+        var sql = 'SELECT * FROM realizacoes';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});

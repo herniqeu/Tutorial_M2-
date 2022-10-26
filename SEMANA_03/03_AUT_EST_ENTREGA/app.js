5d45b072ddea94aaa0d946926ad40c4d647a4965
herniqeu <hhhenriquegodoyyy@gmail.com>
2022-10-26 12:37:50 -0300

    adcionando versão mais atualizada do codigo

--- SEMANA_03/03_AUT_EST_ENTREGA/app.js
+++ SEMANA_03/03_AUT_EST_ENTREGA/app.js
@@ -2,15 +2,15 @@ const express = require('express');
     const app = express();
 
     const hostname = '127.0.0.1';
-    const port = 3000;
+    const port = 3000; 
     const sqlite3 = require('sqlite3').verbose();
-    const DBPATH = 'database.db'; //use o nome que você achar melhor para o banco de dados
+    const DBPATH = 'database.db'; 
 
     app.use(express.json());
     app.get('/dados_pessoais', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var db = new sqlite3.Database(DBPATH); 
         //console.log(db)
         var sql = 'SELECT * FROM dados_pessoais';
         db.all(sql, [],  (err, rows ) => {
@@ -19,13 +19,12 @@ const express = require('express');
             }
             res.json(rows);
         });
-        db.close(); // Fecha o banco
+        db.close(); 
     });
     app.get('/experiencia', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); // Abre o banco
-        //teste de git
+        var db = new sqlite3.Database(DBPATH); 
         var sql = 'SELECT * FROM experiencia';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
@@ -33,12 +32,12 @@ const express = require('express');
             }
             res.json(rows);
         });
-        db.close(); // Fecha o banco
+        db.close(); 
     });
     app.get('/formacao', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var db = new sqlite3.Database(DBPATH); 
         console.log(db)
         var sql = 'SELECT * FROM formacao';
         db.all(sql, [],  (err, rows ) => {
@@ -47,12 +46,12 @@ const express = require('express');
             }
             res.json(rows);
         });
-        db.close(); // Fecha o banco
+        db.close(); 
     });
     app.get('/habilidade', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var db = new sqlite3.Database(DBPATH); 
         console.log(db)
         var sql = 'SELECT * FROM habilidade';
         db.all(sql, [],  (err, rows ) => {
@@ -61,12 +60,12 @@ const express = require('express');
             }
             res.json(rows);
         });
-        db.close(); // Fecha o banco
+        db.close(); 
     });
     app.get('/realizacoes', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var db = new sqlite3.Database(DBPATH); 
         console.log(db)
         var sql = 'SELECT * FROM realizacoes';
         db.all(sql, [],  (err, rows ) => {
@@ -75,7 +74,7 @@ const express = require('express');
             }
             res.json(rows);
         });
-        db.close(); // Fecha o banco
+        db.close(); 
     });
 
 app.listen(port, hostname, () => {

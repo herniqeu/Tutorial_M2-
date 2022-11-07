5081fe4c173b0e89e504e025a03a0d6ae00a07a0
herniqeu <hhhenriquegodoyyy@gmail.com>
2022-11-07 14:23:53 -0300

    atualizando alguns erros :)

--- SEMANA_04/03_AUT_EST_ENTREGA/app.js
+++ SEMANA_04/03_AUT_EST_ENTREGA/app.js
@@ -1,162 +1,100 @@
 const express = require('express'); 
-    const app = express();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-    const hostname = '127.0.0.1';
-    const port = 3000; 
-    const sqlite3 = require('sqlite3').verbose();
-    const DBPATH = 'database.db'; 
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'database.db';
 
-    app.use(express.json());
-    app.get('/dados_pessoais', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); 
-        //console.log(db)
-        var sql = 'SELECT * FROM dados_pessoais';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); 
-    });
-    app.get('/experiencia', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); 
-        var sql = 'SELECT * FROM experiencia';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); 
-    });
-    app.get('/formacao', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); 
-        console.log(db)
-        var sql = 'SELECT * FROM formacao';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); 
-    });
-    app.get('/habilidade', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); 
-        console.log(db)
-        var sql = 'SELECT * FROM habilidade';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); 
-    });
-    app.get('/realizacoes', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
-        var db = new sqlite3.Database(DBPATH); 
-        console.log(db)
-        var sql = 'SELECT * FROM realizacoes';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); 
-    });
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+/* Definição dos endpoints */
+/******** CRUD ************/
+app.use(express.json());
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/experiencia', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM experiencia ORDER BY nome_da_empresa';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+app.post('/insereexperiencia', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "INSERT INTO experiencia (nome_da_empresa, cargo, resumo) VALUES ('" + req.body.nome_da_empresa + "', '" + req.body.cargo + "', '" + req.body.resumo + "')";
+	console.log(sql);
+
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>Inserido!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+app.get('/atualizaexperiencia1', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "SELECT * FROM experiencia WHERE nome_da_empresa ="+ req.query.nome_da_empresa;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/atualizaexperiencia', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "UPDATE experiencia SET  cargo = '" + req.body.cargo + "', resumo='" + req.body.resumo + "' WHERE nome_da_empresa ='" + req.body.nome_da_empresa + "'";
+	console.log(sql);
+	
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	res.write('<p>Atualizado!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.get('/removeexperiencia', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "DELETE FROM experiencia WHERE nome_da_empresa ='" + req.query.nome_da_empresa + "'";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.write('<p>Removido!</p><a href="/">VOLTAR</a>');
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-//------------------------
-
-app.get('/dados_pessoais', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = 'SELECT * FROM dados_pessoais';
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-    db.close(); // Fecha o banco
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
   });
-
-// INSERT experiências
-app.post('/inseredados_pessoais', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    sql = "INSERT INTO experiências (nome, endereco, telefone, email) VALUES ('" + req.body.nome + "', '" + req.body.endereco + "', " + req.body.telefone + "', " + req.body.email + "')";
-    console.log(sql);
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }	
-    });
-    db.close(); // Fecha o banco
-    res.end();
-});
-
-// SELECT experiência conforme o cargo
-   app.get('/selectdados_pessoais', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "SELECT * FROM dados_pessoais WHERE cod_dados_pessoais="+ req.body.cod_dados_pessoais;
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-    db.close(); // Fecha o banco
-});
-
-// UDPATE experiências
-app.put('/atualizadados_pessoais', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "UPDATE OR IGNORE dados_pessoais  SET cod_dados_pessoais='" + req.body.cod_dados_pessoais + "',nome='" + req.body.nome + "', endereco = '" + req.body.endereco + "' , telefone='" + req.body.telefone + "' , email='" + req.body.email + "'" ;
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }
-        res.end();
-    });
-    db.close(); // Fecha o banco
-});
-
-// DELETE experiência
-app.delete('/removedados_pessoais', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "DELETE FROM dados_pessoais WHERE cod_dados_pessoais="+ req.body.cod_dados_pessoais;
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }
-        res.end();
-    });
-    db.close(); // Fecha o banco
-});
